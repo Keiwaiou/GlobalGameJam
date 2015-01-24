72cc5eca998b990e574297dc31e0f688ad3b61b2
Keiwaiou <kristindouglass@vlan811-040-190.pioneernet.du.edu>
2015-01-23 20:43:39 -0700

    this is nothing

---
+++ GGJ_2015/GGJ_2015/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  GGJ_2015
+//
+//  Created by Kris Douglass, Charlie Strong, Lewis Fox, Nathan Saslavsky on 1/23/15.
+//  Copyright (c) 2015 Kristin Douglass. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+
+
+int main(int argc, const char * argv[])
+{
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
